c17b20a6ed35384902c7e3a37aa74fa89b2b1fae
vafmendes <vmendes667@gmail.com>
2023-11-16 04:31:52 -0300

    Altualização no arquivo middlewarejs

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,3 +1,5 @@
+const multer = require('multer');
+const path = require('path');
 //trabalhando com middleware
 exports.middewareGlobal = (req, res, next)=>{
     res.locals.errors = req.flash('errors');
@@ -18,28 +20,17 @@ exports.outroMiddleware = (req, res, next)=>{
 //    next(); 
 // }
 
-exports.checkCsrfError = (err, req, res, next) =>{
-    if(err && err.code){
-     return res.render('../views/includes/404');
-    }
+// exports.checkCsrfError = (err, req, res, next) =>{
+//     if(err && err.code){
+//      return res.render('../views/includes/404');
+//     }
  
-    next(); 
- }
+//     next(); 
+//  }
 
 
-exports.csrfMiddleware = (req, res, next) =>{
-    res.locals.csrfToken = req.csrfToken();
-    next();
-}
+// exports.csrfMiddleware = (req, res, next) =>{
+//     res.locals.csrfToken = req.csrfToken();
+//     next();
+// }
 
-exports.loginRequired = (req, res, next) =>{
-    if(!req.session.user){
-        req.flash('errors', 'Você precisa fazer login');
-        req.session.save(()=>{
-            res.redirect('/');
-        });
-        return;
-    }
-
-    next();
-}
