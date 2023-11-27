89ee892677b4409218cccce8a5769a3a3e5cc257
vafmendes <vmendes667@gmail.com>
2023-11-26 21:27:16 -0300

    Realizada configuração da rota para os arquivos importados utilizando a library 'multer'

--- routes.js
+++ routes.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const route = express.Router();
 const multer = require('multer');
+const path = require('path');
 const homeController = require('./src/controllers/homeController');
 const arquivosController = require('./src/controllers/arquivosController');
+
+const upload = multer({dest: 'uploads/'});
+
+
 
 
 //Rotas da home
 route.get('/', homeController.index);
-route.get('/upload/', arquivosController.index2);
-route.post('/upload/index', arquivosController.upload);
+route.post('/upload', upload.single('csvFile'), arquivosController.upload);
 
 
 module.exports = route;
