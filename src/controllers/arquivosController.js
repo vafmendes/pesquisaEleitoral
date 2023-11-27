ea1338f2947c10b2a5605108fb786b4afc37dc3e
vafmendes <vmendes667@gmail.com>
2023-11-26 21:32:03 -0300

    Feita a leitura e inserção dos dados dos arquivos csv importados para o banco de dados

--- src/controllers/arquivosController.js
+++ src/controllers/arquivosController.js
@@ -1,31 +1,36 @@
 /** ------------------ IMPORTING PACKAGE/MODELS ------------------ **/
-const csvParser = require('csv-parser');
 const Arquivos= require('../models/ArquivosModel');
+const fs = require('fs');
 const path = require('path');
+const csvParser = require('csv-parser');
+const csvToJson = require('csvtojson');
 
-/** ------------------ EXPORTING FUNCTION To upload a file ------------------ **/
-module.exports.index2 = function(req, res){
-    return res.render("../views/upload");
-  }
 
-module.exports.upload = async (req, res) => {
-    const arquivos = new Arquivos(req.body);
-    try{
-        await arquivos.register();
-        if(arquivos.errors.length > 0){
-            req.flash('errors', arquivos.errors);
-            req.session.save(()=>{
-                return res.redirect('back');
-            });
-        }
-        req.flash('success', 'Seus dados foram importados');
-        req.session.file = arquivos.file;
-            req.session.save(() =>{
-                return res.redirect('back');
-            })
+module.exports.upload = async(req, res) => {
+    console.log(req.file);
+    console.log(req.file.originalname);
+    console.log(req.file.destination);
+    // try{
+    //   const csvFile = fs.createReadStream(path.join(__dirname + '..','..','..','uploads',`${req.file.filename}`));
+    //   csvToJson({delimiter: ';'}).fromStream(csvFile).then((json)=>{
+    //     console.log(json);
+    //     let file = Arquivos.insertMany(json); 
+    //   }).catch(e => console.log(e));
+    // }catch(e){
+    //   console.status(400).json(e);
 
-    }catch(e){
-        console.log(e);
-        return res.render('../views/includes/404');
-    }
-}   
+    const datas = [];
+
+    fs.createReadStream(path.join(__dirname + '..','..','..','uploads',`${req.file.filename}`))
+    .pipe( csvParser({separator: ';'||',',mapHeaders: ({ header, index }) => header.toLowerCase()}))
+     .on('data', (data) =>{
+      datas.push(data);
+    })
+    .on('end',async ()=>{
+     console.log(datas);
+     let file = await Arquivos.insertMany(datas);
+   })
+
+
+    return res.send("Ok");
+}
